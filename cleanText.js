12a355cfe9ddbdd014505c33c4261ed6707e8bed
laxman <ramlaxman947@gmail.com>
2022-03-20 20:24:35 +0530

    varietyOfwords function

--- cleanText.js
+++ cleanText.js
@@ -8,9 +8,20 @@ let cleanedText = cleanText(sentence);
 console.log(cleanedText);
 //I am a teacher and I love teaching. There is nothing as more rewarding as educating and empowering people. I found teaching more interesting than any other jobs. Does this motivate you to be a teacher?
 
-function numberOfWords(sentenceCount) {
-  sentenceCount = cleanedText.split(" ").filter((w) => w.length > 1).length;
+function numberOfWords(sentence) {
+  sentenceCount = sentence.split(" ").filter((w) => w.length > 1).length;
   return sentenceCount;
 }
 
 console.log(numberOfWords(cleanedText)); //31
+
+function varietyOfwords(sentence) {
+  sentence = sentence.replace(/[^A-Za-z ]/g, ""); //To Remove . as well as ? (Includes only letters)
+
+  let sentenceUniqueCount = sentence.split(" "); //To Separate words
+
+  sentenceUniqueCount = [...new Set(sentenceUniqueCount)]; //To Remove duplicates
+
+  return sentenceUniqueCount.length;
+}
+console.log(varietyOfwords(cleanedText)); //28
